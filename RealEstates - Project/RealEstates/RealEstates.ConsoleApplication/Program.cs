b896b779195ca87a7f3077a366b5ec48f9a4be6a
nstoyanov <nikolai_kula1@abv.bg>
2020-08-02 20:14:24 +0300

    Implement services and RealEstates.Importer. Add Initial migration

--- RealEstates - Project/RealEstates/RealEstates.ConsoleApplication/Program.cs
+++ RealEstates - Project/RealEstates/RealEstates.ConsoleApplication/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Services.Interfaces;
+using RealEstates.Services.Services;
+using System;
+using System.Text;
 
 namespace RealEstates.ConsoleApplication
 {
@@ -6,7 +11,21 @@ namespace RealEstates.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var db = new RealEstateDbContext();
+            db.Database.Migrate();
+
+            IPropertiesService propertiesService = new PropertiesService(db);
+            propertiesService.Create("Младост", 120, 2020, 100000, "4-СТАЕН", "Панел", 5, 6);
+
+            IDistrictsService districtsService = new DistrictsService(db);
+            var districts = districtsService.GetTopDistrictsByAveragePrice();
+
+            foreach (var district in districts)
+            {
+                Console.WriteLine($"{district.Name} => Price: {district.AveragePrice:f2} ({district.MinPrice:f2}-{district.MaxPrice:f2}) => {district.PropertiesCount} properties");
+            }
         }
     }
 }
